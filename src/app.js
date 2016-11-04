20b1af3092b5a2ebea375f8a55b5e91f6584b8f4
Daniel Wilson-Thomas <daniel@mojotech.com>
2016-11-04 16:47:59 -0400

    Set up build chain and simple sources.
    Hello World!

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const foo = 1;
+
+ReactDOM.render(<div>Hello World {foo}</div>, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: './bin',
+    filename: 'app.bundle.js'
+  },
+
+  // Dev server configuration
+  devServer: {
+    hot: true,
+    inline: true,
+    host: 'localhost',
+    port: 3000,
+    contentBase: './src'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.ejs']
+  },
+
+  // Plugins
+  plugins: [
+    new webpack.NoErrorsPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/index.ejs',
+      hash: true
+    })
+  ],
+
+  // General configuration
+  debug: true,
+  devtool: 'cheap-eval-source-map'
+};
