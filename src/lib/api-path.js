8ce44c653999cd8fe98c2c60a2b065ce321b0c8c
Daniel Wilson-Thomas <daniel@mojotech.com>
2016-11-05 14:55:15 -0400

    Fetch session information when joined
    Some things to note:
    
    * I'm very happy with the way the `onmessage` callback decomposed.
    Everything ended up nice and self-contained.
    * The `action` helper is proving really useful. This is because object
    construction in Javascript isn't first-class or easily composable. The
    helper turns the object construction into a function, which fixes
    those issues.

--- src/lib/api-path.js
+++ src/lib/api-path.js
@@ -1,3 +1,4 @@
 export const apiPath = 'http://localhost:8080';
+export const wsPath = 'ws://localhost:8080';
 
 export default apiPath;
--- src/session/index.js
+++ src/session/index.js
@@ -1,18 +1,32 @@
 import { action, SESSION } from '../types/common';
 import { changeScreen } from '../change-screens';
-import { curry } from 'ramda';
-import { apiPath } from '../lib/api-path';
+import { compose, prop, curry } from 'ramda';
+import { apiPath, wsPath } from '../lib/api-path';
+import requestJson from '../lib/request-json';
 
 const initialState = {
-  id: null
+  id: null,
+  name: null,
+  locked: null,
+  loading: true,
+  socket: null,
+  answerer: {
+    name: null
+  },
+  questioners: [],
+  questions: []
 };
 
 const session = (state=initialState, action) => {
   switch(action.type) {
   case 'session/join':
     action.sideEffect((d) => d(changeScreen(SESSION)));
-    action.sideEffect(doJoinSession(action.payload.sessionId, action.payload.name));
+    action.sideEffect(joinAndFetchSession(action.payload.sessionId, action.payload.name));
     return { ...initialState, id: action.payload.sessionId };
+  case 'session/load':
+    return { ...state, ...action.payload };
+  case 'session/set_socket':
+    return { ...state, socket: action.payload };
   default:
     return state;
   }
@@ -22,7 +36,7 @@ export function joinSession(sessionId, name) {
   return action('session/join', { sessionId, name });
 }
 
-const doJoinSession = curry((sessionId, name, dispatch) => {
+const joinRequest = (sessionId, name) => {
   const headers = new Headers({ 'Content-type': 'application/json', 'Accept': 'application/json' });
 
   return fetch(`${apiPath}/sessions/${sessionId}/join`, {
@@ -32,6 +46,41 @@ const doJoinSession = curry((sessionId, name, dispatch) => {
     headers: headers,
     body: JSON.stringify({ questionerName: name })
   });
+};
+
+const joinAndFetchSession = curry((sessionId, name, dispatch) => {
+  return joinRequest(sessionId, name)
+    .then(requestJson)
+    .then(() => {
+      // store socket on state so it doesn't get garbage collected
+      dispatch(action('session/set_socket', openSessionSocket(sessionId, dispatch)));
+    });
 });
+
+function openSessionSocket(sessionId, dispatch) {
+  const socket = new WebSocket(`${wsPath}/sessions/${sessionId}/messages`);
+  socket.onmessage = compose(dispatch, handleMessage, parseMessage);
+  return socket;
+}
+
+const parseMessage = compose(JSON.parse, prop('data'));
+
+function handleMessage(message) {
+  switch(message.tag) {
+  case 'SessionState':
+    return action('session/load', toSessionInfo(message.session));
+  }
+}
+
+function toSessionInfo({ session, answerer, questioners, questions }) {
+  return {
+    id: session.sessionId,
+    name: session.name,
+    locked: session.locked,
+    answerer,
+    questions,
+    questioners
+  };
+}
 
 export default session;
