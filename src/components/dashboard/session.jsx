a8fcc10353b49ab0607d6f9f85720961d808fc53
Daniel Wilson-Thomas <daniel@mojotech.com>
2017-01-08 23:46:26 -0500

    Move dashboard session description out of form

--- src/components/dashboard/session.jsx
+++ src/components/dashboard/session.jsx
@@ -4,19 +4,21 @@ import preventDefault from 'lib/prevent-default';
 import { compose, propOr } from 'ramda';
 import StatefulForm from 'components/stateful-form';
 
-const DashboardSession = ({ session, answerer, totals, joinSession }) => (
-  <StatefulForm form={({ onSubmit, onChange, name='' }) => (
-    <form onSubmit={compose(joinSession(session.sessionId), propOr(null, 'name'), preventDefault(onSubmit))}>
-      <p>
-        {session.name}, {session.locked}, {answerer.name}, {totals.questioners} Questioners, {totals.answeredQuestions}/{totals.questions} Questions Answered
-      </p>
-      <p>
-        <label htmlFor='name'>Desired Name</label>
-        <input value={name} type='text' placeholder='Dexter' onChange={onChange('name')} />
-      </p>
-      <button type='submit'>Join Session</button>
-    </form>
-  )} />
+const DashboardSession = ({ session, answerer, totals, joinSession}) => (
+  <div>
+    <p>
+      {session.name}, {session.locked}, {answerer.name}, {totals.questioners} Questioners, {totals.answeredQuestions}/{totals.questions} Questions Answered
+    </p>
+    <StatefulForm form={({ onSubmit, onChange, name='' }) => (
+      <form onSubmit={compose(joinSession(session.sessionId), propOr(null, 'name'), preventDefault(onSubmit))}>
+        <p>
+          <label htmlFor='name'>Desired Name</label>
+          <input value={name} type='text' placeholder='Dexter' onChange={onChange('name')} />
+        </p>
+        <button type='submit'>Join Session</button>
+      </form>
+    )} />
+  </div>
 );
 
 DashboardSession.propTypes = {
