b98dc5210c01e03b5ad18473232567518137ddac
Daniel Wilson-Thomas <daniel@mojotech.com>
2017-01-15 10:17:59 -0500

    Poll for new sessions when on the dashboard

--- src/reducers/dashboard/index.js
+++ src/reducers/dashboard/index.js
@@ -1,11 +1,13 @@
-import { compose, filter, reduce, map } from 'ramda';
-import { action } from 'types/common';
+import { curry, compose, filter, reduce, map } from 'ramda';
+import { action, DASHBOARD } from 'types/common';
 import apiPath from 'lib/api-path';
 import requestJson from 'lib/request-json';
+import { CHANGE_SCREEN, getScreen } from '../change-screens';
 
 const initialState = {
   sessions: [],
-  loading: true
+  loading: true,
+  interval: null
 };
 
 const dashboard = (state = initialState, action) => {
@@ -15,14 +17,33 @@ const dashboard = (state = initialState, action) => {
   case 'dashboard/reset':
     action.sideEffect(fetchDashboard);
     return initialState;
+  case 'dashboard/set-interval':
+    return { ...state, interval: action.payload };
+  case CHANGE_SCREEN:
+    if (getScreen(action) !== DASHBOARD) {
+      action.sideEffect(clearInterval(state.interval));
+    }
+    return state;
   default:
     return state;
   }
 };
 
+const clearInterval = curry((interval, dispatch) => {
+  clearInterval(interval);
+  dispatch(action('dashboard/set-interval', null));
+})
+
 export const dashboardReset = action('dashboard/reset', null);
 
 export function fetchDashboard(dispatch) {
+  const interval = setInterval(() => dashboardRequest(dispatch), 30 * 1000);
+
+  dispatch(action('dashboard/set-interval', interval));
+  dashboardRequest(dispatch);
+}
+
+function dashboardRequest(dispatch) {
   return fetch(`${apiPath}/sessions`)
     .then(requestJson)
     .then(compose(dispatch, action('dashboard/load-sessions'), map(toDashboardSessionInfo)));
