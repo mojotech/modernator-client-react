c7b03bd74fc8af069f7229b4a970e21c4cbd5cb6
Daniel Wilson-Thomas <daniel@mojotech.com>
2017-01-30 22:30:05 -0500

    Set up less css loader for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const LessPluginAutoPrefix = require('less-plugin-autoprefix');
 
 module.exports = {
   entry: './src/app.js',
@@ -11,6 +12,17 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ['babel-loader']
+      },
+      {
+        test: /\.less$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          {
+            loader: 'less-loader',
+            options: { plugins: [ new LessPluginAutoPrefix({ browsers: [ "last 2 versions" ] }) ] }
+          }
+        ]
       }
     ]
   },
@@ -20,7 +32,8 @@ module.exports = {
       components: path.resolve('./src/components'),
       lib: path.resolve('./src/lib'),
       types: path.resolve('./src/types'),
-      reducers: path.resolve('./src/reducers')
+      reducers: path.resolve('./src/reducers'),
+      styles: path.resolve('./src/styles')
     }
   },
 
