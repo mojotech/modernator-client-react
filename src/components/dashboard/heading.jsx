9dff6951e74d07f968d7d9830a2fd67dfd3a544f
Daniel Wilson-Thomas <daniel@mojotech.com>
2017-05-28 19:45:01 -0400

    Style links with .btn classes

--- src/components/dashboard/heading.jsx
+++ src/components/dashboard/heading.jsx
@@ -4,7 +4,7 @@ import { Link } from 'redux-little-router';
 
 const Heading = () => (
   <div className='heading'>
-    <Link className='new-session' href={newSession()}>New Session</Link>
+    <Link className='new-session btn' href={newSession()}>New Session</Link>
     <h1 className='h1'>Modernator</h1>
   </div>
 );
