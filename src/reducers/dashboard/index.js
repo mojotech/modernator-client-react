95a7e7980ffe7a1a6d1b1a9ac8a21d5c07fa9609
Daniel Wilson-Thomas <daniel@mojotech.com>
2017-05-29 21:41:58 -0400

    Perform an explicit dashboard reset so the action can be listened for in other reducers

--- src/reducers/dashboard/index.js
+++ src/reducers/dashboard/index.js
@@ -12,10 +12,15 @@ const initialState = {
   interval: null
 };
 
+export const DASHBOARD_RESET = 'dashboard/reset';
+
 const dashboard = (state = initialState, action) => {
   switch(action.type) {
   case 'dashboard/load-sessions':
     return { ...state, loading: false, sessions: action.payload };
+  case DASHBOARD_RESET:
+    action.sideEffect(fetchDashboard);
+    return initialState;
   case 'dashboard/set-interval':
     return { ...state, interval: action.payload };
   case LOCATION_CHANGED:
@@ -23,13 +28,15 @@ const dashboard = (state = initialState, action) => {
       action.sideEffect(clearInterval(state.interval));
       return state;
     } else {
-      action.sideEffect(fetchDashboard);
-      return initialState;
+      action.sideEffect(dashboardReset);
+      return state;
     }
   default:
     return state;
   }
 };
+
+const dashboardReset = (d) => d(action(DASHBOARD_RESET, null));
 
 const clearInterval = curry((interval, dispatch) => {
   window.clearInterval(interval);
