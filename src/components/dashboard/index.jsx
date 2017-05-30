eccba890754ce18b57b7d3e35b3b4983eff0be8e
Daniel Wilson-Thomas <daniel@mojotech.com>
2017-05-29 21:55:16 -0400

    Only show session join buttons if a user is signed in

--- src/components/dashboard/index.jsx
+++ src/components/dashboard/index.jsx
@@ -5,13 +5,14 @@ import { DashboardSession } from 'types/prop-types';
 import Session from './session';
 import Heading from './heading';
 import { curry, compose, values, reject, contains, concat, isEmpty } from 'ramda';
+import { isSignedIn } from 'types/user';
 require('styles/dashboard.less');
 
-const SessionList = ({ sessions, joinSession }) => (
+const SessionList = ({ sessions, joinSession, showJoin }) => (
   <ul className='session-list'>
     {sessions.map((session) =>
       <li key={session.session.sessionId}>
-        <Session {...session} joinSession={joinSession} />
+        <Session {...session} joinSession={joinSession} showJoin={showJoin} />
       </li>
     )}
   </ul>
@@ -24,19 +25,19 @@ const Dashboard = ({ questionerSessions, answererSessions, otherSessions, loadin
     {answererSessions.length > 0 &&
       <div key='answerer' className='answerer-sessions'>
         <h2 className='h2'>Answerer</h2>
-        <SessionList sessions={answererSessions} joinSession={joinSession} />
+        <SessionList sessions={answererSessions} joinSession={joinSession} showJoin={isSignedIn(user)} />
       </div>
     }
     {questionerSessions.length > 0 &&
       <div key='questioner' className='questioner-sessions'>
         <h2 className='h2'>Questioner</h2>
-        <SessionList sessions={questionerSessions} joinSession={joinSession} />
+        <SessionList sessions={questionerSessions} joinSession={joinSession} showJoin={isSignedIn(user)} />
       </div>
     }
     {otherSessions.length > 0 &&
       <div key='other' className='other-sessions'>
         <h2 className='h2'>Unjoined</h2>
-        <SessionList sessions={otherSessions} joinSession={joinSession} />
+        <SessionList sessions={otherSessions} joinSession={joinSession} showJoin={isSignedIn(user)} />
       </div>
     }
   </div>
--- src/components/dashboard/session.jsx
+++ src/components/dashboard/session.jsx
@@ -4,7 +4,7 @@ import preventDefault from 'lib/prevent-default';
 import { compose, propOr } from 'ramda';
 import StatefulForm from 'components/stateful-form';
 
-const DashboardSession = ({ session, answerer, totals, joinSession }) => (
+const DashboardSession = ({ session, answerer, totals, showJoin=false, joinSession }) => (
   <div className='session'>
     <div className='session-info'>
       <span className='session-name'>{session.name}</span> by
@@ -13,19 +13,22 @@ const DashboardSession = ({ session, answerer, totals, joinSession }) => (
         {totals.questioners} Questioners, {totals.answeredQuestions}/{totals.questions} Questions Answered
       </p>
     </div>
-    <StatefulForm form={({ onSubmit, onChange }) => (
-      <form className='session-join' onSubmit={compose(joinSession(session.sessionId), preventDefault(onSubmit))}>
-        <div className='actions'>
-          <button className='submit' type='submit'>Join</button>
-        </div>
-      </form>
-    )} />
+    {showJoin &&
+      <StatefulForm form={({ onSubmit, onChange }) => (
+        <form className='session-join' onSubmit={compose(joinSession(session.sessionId), preventDefault(onSubmit))}>
+          <div className='actions'>
+            <button className='submit' type='submit'>Join</button>
+          </div>
+        </form>
+      )} />
+    }
   </div>
 );
 
 DashboardSession.propTypes = {
   ...dashboardSessionShape,
-  joinSession: React.PropTypes.func.isRequired
+  joinSession: React.PropTypes.func.isRequired,
+  showJoin: React.PropTypes.bool.isRequired
 };
 
 export default DashboardSession;
