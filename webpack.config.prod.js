402012dbe8afe62f3eb606b453f65b43a9bcc57b
Daniel Wilson-Thomas <daniel@mojotech.com>
2017-06-22 13:28:06 -0400

    Use the absolute path for output

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,11 +1,12 @@
 const webpack = require('webpack');
 const R = require('ramda');
 const baseConfig = require('./webpack.config');
+const path = require('path');
 require('dotenv').config();
 
 module.exports = R.merge(baseConfig, {
   output: {
-    path: './bin/dist',
+    path: path.resolve('./bin/dist'),
     filename: 'app.bundle.prod.js'
   },
   plugins: baseConfig.plugins.concat(
